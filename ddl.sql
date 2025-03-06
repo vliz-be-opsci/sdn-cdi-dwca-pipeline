0179e21a619e059d33b82bc759c7129d3380887e
Radermecker Cyril <cyril.radermecker@vliz.be>
2025-03-06 15:33:26 +0100

    Update ddl.sql
    Added queries that have been tested recently

--- ddl.sql
+++ ddl.sql
@@ -307,4 +307,84 @@ VALUES (
 	'FirstName LastName',
 	'firstname.lastname@vliz.be'
 );
-
+--------------------------------------------------------------------------------
+INSERT INTO jobs (
+	name,
+	active,
+	order_placed,
+	retrigger,
+	query,
+	last_run,
+	last_data_file,
+	last_meta_file,
+	order_id,
+	owner,
+	owner_email
+)
+VALUES (
+	'Pilot_Mediterranean_Sea',
+	1,
+	0,
+	0,
+	'{"north":45.7833,"east":36.2173,"south":30.2639,"west":-6.0327,"inside":1,"parameters_p08":"ds01"}',
+	'1900-01-01 00:00:00.00',
+	'',
+	'',
+	'',
+	'FirstName LastName',
+	'firstname.lastname@vliz.be'
+);
+--------------------------------------------------------------------------------
+INSERT INTO jobs (
+	name,
+	active,
+	order_placed,
+	retrigger,
+	query,
+	last_run,
+	last_data_file,
+	last_meta_file,
+	order_id,
+	owner,
+	owner_email
+)
+VALUES (
+	'Pilot_Black_Sea',
+	1,
+	0,
+	0,
+	'{"north":47.3103,"east":42.355,"south":40.9088,"west":27.4437,"inside":1,"parameters_p08":"ds01"}',
+	'1900-01-01 00:00:00.00',
+	'',
+	'',
+	'',
+	'FirstName LastName',
+	'firstname.lastname@vliz.be'
+);
+--------------------------------------------------------------------------------
+INSERT INTO jobs (
+	name,
+	active,
+	order_placed,
+	retrigger,
+	query,
+	last_run,
+	last_data_file,
+	last_meta_file,
+	order_id,
+	owner,
+	owner_email
+)
+VALUES (
+	'Pilot_all_BODV',
+	1,
+	0,
+	0,
+	'{"parameters_p08":"ds01"}',
+	'1900-01-01 00:00:00.00',
+	'',
+	'',
+	'',
+	'FirstName LastName',
+	'firstname.lastname@vliz.be'
+);
